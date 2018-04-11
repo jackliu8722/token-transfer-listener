54b383689c49f3db0239d97f0fda6184a651ed9e
jackliu8722 <125342620@qq.com>
2018-04-11 10:06:04 +0800

    fix null pointer

--- src/test/java/com/ethereum/token/transfer/listener/Web3ServiceTest.java
+++ src/test/java/com/ethereum/token/transfer/listener/Web3ServiceTest.java
@@ -10,7 +10,7 @@ public class Web3ServiceTest {
     @Test
     public void test(){
         Web3jService service = new Web3jService();
-        service.init(4851647,"http://35.194.247.139:8545","0x5A9bF6bADCd24Fe0d58E1087290c2FE2c728736a",10);
+        service.init(5418583,"https://mainnet.infura.io/AtQQwJGCuEE2BRkSZgi5","0x5A9bF6bADCd24Fe0d58E1087290c2FE2c728736a",10);
 //        service.start((from,to,value,time,fromValue,toValue,txHash) -> {
 //            System.out.println("from:" + from + ", to:" + to + ", value:" + value.doubleValue() + ", time:" + time
 //            + ", fromValue:" + fromValue.doubleValue() + ", toValue:" + toValue.doubleValue() + ", txHash:" + txHash);
